e6ee348cd8619787fb6d5bdff1de6266ccd20752
Moth <acrownofmoths@gmail.com>
2024-04-30 18:40:07 +1200

    starting sessions

---
+++ Godot/scripts/resources/Session.gd
@@ -0,0 +1,7 @@
+class_name Session extends Resource
+
+@export var players : Array
+@export var current_deck : Deck
+@export var items_on_table : Array[Card]
+@export var actors_on_table : Array[Card]
+@export var current_terrain
