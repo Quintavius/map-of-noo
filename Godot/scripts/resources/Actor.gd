8648a8c014f2b1808413cbcbfe55d534a3cb76c2
Moth <acrownofmoths@gmail.com>
2024-06-24 21:09:39 +1200

    picture frame file

--- Godot/scripts/resources/Actor.gd
+++ Godot/scripts/resources/Actor.gd
@@ -4,6 +4,8 @@ enum Actor_State {Normal, KO, Dead}
 
 @export var state : Actor_State
 
+@export var pronouns : String
+
 @export var brains : Dictionary = {level = 0, experience = 0 }
 @export var brawn : Dictionary = {level = 0, experience = 0 }
 @export var tongue : Dictionary = {level = 0, experience = 0 }
