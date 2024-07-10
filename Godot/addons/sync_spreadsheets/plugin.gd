8e991a3cd1105307f139cec2b6250b7d4c7aae98
Moth <acrownofmoths@gmail.com>
2024-07-10 12:41:53 +1200

    CSV Addons

---
+++ Godot/addons/sync_spreadsheets/plugin.gd
@@ -0,0 +1,17 @@
+@tool
+extends EditorPlugin
+var sync_instance : Node
+
+func _enter_tree():
+	# So that the scene reloads everytime we activate the plugin, allowing for quicker prototyping.
+	var packed : PackedScene = load("res://addons/sync_spreadsheets/files/csv_sync.tscn")
+	sync_instance = packed.instantiate()
+	sync_instance.should_setup = true
+	# Not available in godot 4.2
+	# add_control_to_bottom_panel(sync_instance, "Sync CSV Spreadsheets", Shortcut.new())
+	add_control_to_bottom_panel(sync_instance, "Sync CSV")
+
+func _exit_tree():
+	if sync_instance:
+		remove_control_from_bottom_panel(sync_instance)
+		sync_instance.queue_free()
